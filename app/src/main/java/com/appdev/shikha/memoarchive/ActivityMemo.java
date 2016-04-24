a8c0103e0cf866c347cfae9d31e761745027fe35
shika02 <kaushik.shikhs02@gmail.com>
2016-04-24 11:34:30 +0530

    An app to remind you important things that u want to accomplish ....:)

---
+++ app/src/main/java/com/appdev/shikha/memoarchive/ActivityMemo.java
@@ -0,0 +1,13 @@
+package com.appdev.shikha.memoarchive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+    }
+}
