d84d7e7ff4f3b408748d7b36d62b12c8490e342b
shika02 <kaushik.shikhs02@gmail.com>
2016-05-08 03:03:13 +0530

    A button and image view is added in new xml file and is included in main xml layout

--- app/src/main/java/com/appdev/shikha/memoarchive/ActivityMemo.java
+++ app/src/main/java/com/appdev/shikha/memoarchive/ActivityMemo.java
@@ -2,12 +2,15 @@ package com.appdev.shikha.memoarchive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMemo extends AppCompatActivity {
+    Toolbar mtool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo);
+        mtool=(Toolbar)findViewById(R.id.toolbar);
     }
 }
